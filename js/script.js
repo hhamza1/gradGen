fc863d3cbf93a43142514bdc630c01b50144d2b1
unknown <hamzahmitou@gmail.com>
2019-07-24 13:48:12 +0300

    IInıitial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var gradCol = document.querySelector("h3");
+var firstColor = document.querySelector(".fColor");
+var secondColor = document.querySelector(".sColor");
+
+console.log(gradCol);
+console.log(firstColor);
+console.log(secondColor);
