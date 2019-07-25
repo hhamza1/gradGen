b4b458e53c5a359f63ffa7aec347caae35b2dbb2
unknown <hamzahmitou@gmail.com>
2019-07-25 16:50:07 +0300

    Displaying the linear-gradient

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+var body = document.getElementsByTagName("body")[0];
 var gradCol = document.querySelector("h3");
 var firstColor = document.querySelector(".fColor");
 var secondColor = document.querySelector(".sColor");
 
-console.log(gradCol);
-console.log(firstColor);
-console.log(secondColor);
+function setGradColors() {
+    body.style.background = "linear-gradient(to right," + firstColor.value + "," + secondColor.value + ")";
+    gradCol.textContent = body.style.background + ";";
+}
+
+firstColor.addEventListener("change", setGradColors);
+secondColor.addEventListener("change", setGradColors);
